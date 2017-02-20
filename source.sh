af4e808cebdce20fb542dadef6a6f9574f1b8aa1
ingalls <nicholas.ingalls@gmail.com>
2017-02-20 12:20:04 -0500

    Add GeoJSON source

--- source.sh
+++ source.sh
@@ -10,30 +10,35 @@ if [[ $(jq -r -c '.coverage | .country' $SOURCE) != "us" ]]; then
     exit;
 fi
 
+# GeoJSON tag overrides anything else
+GEOJSON=$(jq -r -c '.coverage | ."geometry"' $SOURCE)
+if [[ $GEOJSON != "null" ]]; then
+    echo "{ \"type\": \"Feature\", \"properties\": {}, \"geometry\": $GEOJSON }" >> $(dirname $0)/map/geom.geojson
+    exit
+fi
+
 # Render States
 if [[ $(jq '.coverage | ."US Census" | .name' $SOURCE) == "null" ]] \
     && [[ $(jq '.coverage | ."US Census" | .state' $SOURCE) != "null" ]]
 then
     echo "ok - is a state"
     GEOID=$(jq -r -c '.coverage | ."US Census" | .geoid' $SOURCE)
-    echo "$GEOID,yes" >> $(dirname $0)/map/geoid.csv
+    if [[ $GEOID != "null" ]]; then
+        echo "$GEOID,yes" >> $(dirname $0)/map/geoid.csv
+        exit
+    fi
     exit;
 fi
 
 # Render Counties
 if [[ $(jq '.coverage | .county' $SOURCE) == "null" ]]; then
-    echo "ok - not a county, skipping"
+    echo "ok - not a county"
+
     exit;
 fi
 
 GEOID=$(jq -r -c '.coverage | ."US Census" | .geoid' $SOURCE)
 if [[ $GEOID != "null" ]]; then
     echo "$GEOID,yes" >> $(dirname $0)/map/geoid.csv
-    exit
-fi
-
-GEOJSON=$(jq -r -c '.coverage | ."geometry"' $SOURCE)
-if [[ $GEOJSON != "null" ]]; then
-    echo "{ \"type\": \"Feature\", \"properties\": {}, \"geometry\": $GEOJSON }" >> $(dirname $0)/map/geom.geojson
     exit
 fi
