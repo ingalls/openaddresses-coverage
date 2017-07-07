fa840ce51851e62b8b20e1f9ea3703bcd964e611
ingalls <nick@mapbox.com>
2017-07-07 13:24:19 -0400

    Proper headers after sort

--- map.sh
+++ map.sh
@@ -8,8 +8,8 @@ else
     git clone https://github.com/openaddresses/openaddresses.git /tmp/openaddresses
 fi
 
-echo "GEOID,DONE" > $(dirname $0)/map/geoid.csv
-echo "GEOID,DONE" > $(dirname $0)/map/cageoid.csv
+echo "" > $(dirname $0)/map/geoid.csv
+echo "" > $(dirname $0)/map/cageoid.csv
 echo '{ "type": "FeatureCollection", "features": [' > $(dirname $0)/map/geom.geojson
 
 parallel ./source.sh {} ::: $(find /tmp/openaddresses/sources/us/* -name "*.json")
@@ -21,3 +21,9 @@ echo "]}" >> $(dirname $0)/map/geom.geojson
 
 sort $(dirname $0)/map/geoid.csv -o $(dirname $0)/map/geoid.csv
 sort $(dirname $0)/map/cageoid.csv -o $(dirname $0)/map/cageoid.csv
+
+cat $(dirname $0)/map/header $(dirname $0)/map/geoid.csv > /tmp/geoid.csv
+mv /tmp/geoid.csv $(dirname $0)/map/geoid.csv
+
+cat $(dirname $0)/map/header $(dirname $0)/map/cageoid.csv > /tmp/cageoid.csv
+mv /tmp/cageoid.csv $(dirname $0)/map/cageoid.csv
