0af9b2e5344e089d2d0c96e37568372dc5ce6802
ingalls <ingalls@protonmail.com>
2017-11-21 13:57:16 -0500

    Additional Rebuild

--- map.sh
+++ map.sh
@@ -15,8 +15,8 @@ echo '{ "type": "FeatureCollection", "features": [' > $(dirname $0)/map/geom.geo
 parallel ./source.sh {} ::: $(find /tmp/openaddresses/sources/us/* -name "*.json")
 parallel ./source.sh {} ::: $(find /tmp/openaddresses/sources/ca/* -name "*.json")
 
-sed -i '' 's/}$/},/' $(dirname $0)/map/geom.geojson
-sed -i '' '$ s/.$//' $(dirname $0)/map/geom.geojson
+sed -i 's/}$/},/' $(dirname $0)/map/geom.geojson
+sed -i '$ s/.$//' $(dirname $0)/map/geom.geojson
 echo "]}" >> $(dirname $0)/map/geom.geojson
 
 sort $(dirname $0)/map/geoid.csv -o $(dirname $0)/map/geoid.csv
